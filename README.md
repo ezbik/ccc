c3d50f2a618bbef97c41485b40eb821b97ea53ad
Erik Zbik <erik.zbik@gmail.com>
2013-12-18 14:38:36 +0100

    added more scripts.

--- README.md
+++ README.md
@@ -13,5 +13,37 @@ $ ccc 45 czk USD 2012-02-02
 
 It uses free API http://currencies.apps.grandtrunk.net/ and maintains local cache in ~/.ccc/ in CSV format
 
+Finance tools
+===
 
+**prerequesites** put ccc in your $PATH. Make sure ambar.txt is in same folder as the scripts described below.
+
+**ambar.txt** contains lines to analyze. Each line has the following format:
+```
+2013-12-22      home            9.94$     lamps
+YYYY-MM-DD      type            sum     option notes
+```
+empty lines or lines starting from # are ignored.  Sum must be like 34czk or 2.8$ . If currency not specified then it is assumed BYR. Notes are really optional.
+
+**stats_daily** will show daily report for each day of the *current* month. Sample usage:
+```
+$ ./stats_daily
+2013-12-21 3.99$         food;
+2013-12-22 16.00$        home; gadgets;
+------------------------------
+2013-12 19.99$
+```
+
+**stats_by_type** will show spendings per type, for specified month. If month omitted, then current month is assumed:
+```
+$ ./stats_by_type  2013-12
+
+== month 2013-12 stats by type
+
+3.99$ ( 19.96% ) food
+6.00$ ( 30.02% ) gadgets
+10.00$ ( 50.03% ) home
+```
+
+**convert_to_USD.pl** will convert non-USD currencies to USD.
 
---
+++ convert_to_USD.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+$APPLY=$ARGV[0];
+
+#sub usage { print "Usage: $0 [--apply|-a]\n"; exit }
+
+sub ccc_my {
+
+my $src=shift;
+# src can be
+#       29000   =considered BYR
+#       29czk   =considered CZK
+my $date=shift;
+
+if ($src=~/([\d\.]+)(\D+)/)
+        {
+        $src_amo=$1;
+        $src_cur=$2;
+        }
+elsif ($src=~/([\d\.]+)/)
+        {
+        $src_amo=$1;
+        $src_cur='byr';
+        }
+else { print "wrong currency $src\n"; exit }
+
+        my $ccc_run="ccc $src_amo $src_cur usd $date";
+        my $ccc=`$ccc_run`;
+        chomp $ccc;
+        if ($ccc =~ /^[\d\.]+$/ ) { return $ccc }
+                else { print "bad result '$ccc' from: \`$ccc_run\`\n"; exit }
+}
+
+
+sub  perl_reg {
+
+my $in=shift;
+my $out=shift;
+
+open IN, "<$in";
+open OUT, ">$out";
+while(<IN>)
+        {
+        chomp;
+        $out=$_;
+        if (/[\d\.]+\$/) 
+                { # dollar sign detected; no actions
+                } 
+                else 
+                {
+                $out=~s@^(\d{4}\-\d{2}\-\d{2})(\s+\S+\s+)([\d\.\S]+)(.*)@"$1"."$2".ccc_my($3,$1)."\$".$4@e
+                } 
+        print OUT $out,"\n";
+        }
+
+close IN;
+close OUT;
+
+}
+
+$TMP="/tmp/a.tmp";
+perl_reg("ambar.txt", $TMP);
+
+system ("diff -Naur ambar.txt $TMP");
+
+if ( $APPLY =~/(-a|--apply)/  ) 
+        { 
+        system ("mv -v $TMP ambar.txt") 
+        } 
+else 
+        { 
+        print "\n\npass '-a' arguement to apply changes\n";
+        unlink ($TMP) 
+        }
+	
+
+
+
